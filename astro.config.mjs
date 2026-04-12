518f03aec91617f7e9ddb0f5f702977f2b5ff014
momo-OwO-qwq <momo_owo@hotmail.com>
2026-04-12 11:59:32 +0800

    ✨ 更新日志结构

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,48 +4,51 @@ import starlight from '@astrojs/starlight';
 import starlightScrollToTop from 'starlight-scroll-to-top';
 import starlightImageZoomPlugin from 'starlight-image-zoom';
 
-
 // https://astro.build/config
 export default defineConfig({
-	outDir: "./build",
-	integrations: [
-		starlight({
-			plugins: [starlightScrollToTop(), starlightImageZoomPlugin(),],
-			customCss:['./src/styles/global.css',],
-			title: 'potatos',
-			favicon: '/src/assets/potato.png',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-            defaultLocale: 'root',
-            locales: {root: { label: '简体中文', lang: 'zh-CN',},},
-			sidebar: [
-				{
-					label: '介绍',
-					items: [
-						{ label: '简介', slug: 'introduction/introduction' },
-						{ label:'规则', slug: 'introduction/rules' },
-					],
-				},
-				{
-					label: '公告',
-					items: [
-						{ label: '公告', slug: 'announcement/announcement' },
-						{ label: '日志', slug: 'announcement/log/log' },
-
-					],
-				},
-				{
-					label: '社区',
-					items: [
-						{ label: '社区', slug: 'community/community' },
-					],
-				},
-				{
-					label: '帮助',
-					items: [
-						{ label: '帮助', slug: 'help/help' },
-					],
-				}
-			],
-		}),
-	],
+  outDir: "./build",
+  integrations: [
+    starlight({
+      plugins: [starlightScrollToTop(), starlightImageZoomPlugin()],
+      customCss: ['./src/styles/global.css'],
+      title: 'potatos',
+      favicon: '/src/assets/potato.png',
+      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+      defaultLocale: 'root',
+      locales: { root: { label: '简体中文', lang: 'zh-CN' } },
+      sidebar: [
+        {
+          label: '介绍',
+          items: [
+            { label: '简介', slug: 'introduction/introduction' },
+            { label: '规则', slug: 'introduction/rules' },
+          ],
+        },
+        {
+          label: '公告',
+          items: [
+            { label: '公告', slug: 'announcement/announcement' },
+            {
+              label: '日志',
+              items: [
+                { label: '2026年', slug: 'announcement/log/2026' },
+              ],
+            },
+          ],
+        },
+        {
+          label: '社区',
+          items: [
+            { label: '社区', slug: 'community/community' },
+          ],
+        },
+        {
+          label: '帮助',
+          items: [
+            { label: '帮助', slug: 'help/help' },
+          ],
+        },
+      ],
+    }),
+  ],
 });
